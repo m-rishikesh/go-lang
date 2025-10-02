fcdacfcaa6889a4d8dde120d9eda6bc340202882
raj@deb <mee.rishikeshraj@gmail.com>
2025-10-02 09:31:38 +0000

    adds go.txt

--- mymodule/utils/utils.go
+++ mymodule/utils/utils.go
@@ -1,7 +1,37 @@
 package utils
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func SayHello(name string) {
-	fmt.Println(name + ", Hello")
+func SayHello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("NAME REQUIRED")
+	}
+	msg := fmt.Sprintln(randfunc(), name)
+	return msg, nil
+}
+
+func randfunc() string {
+	formats := [](string){
+		"Hi, Welcome!",
+		"Great to see you, !",
+		"Hail,  Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
+
+func SayHelloArray(name []string) (map[string]string, error) {
+	message := make(map[string]string, len(name))
+	for _, val := range name {
+		returnval, err := SayHello(val)
+		if err != nil {
+			return map[string]string{}, err
+		}
+		message[val] = returnval
+	}
+	fmt.Println("Running the sayhellowfunc")
+	return message, nil
 }
--- new/go.mod
+++ new/go.mod
@@ -2,7 +2,6 @@ module new
 
 go 1.25.1
 
-require github.com/m-rishikesh/go-lang/mymodule v0.0.0-20251002045229-0c0da5a43951
+require github.com/m-rishikesh/go-lang/mymodule v0.0.0-20251002051433-b6fcaab8e5b9
 
 // replace github.com/m-rishikesh/go-lang/mymodule => ../mymodule
-
